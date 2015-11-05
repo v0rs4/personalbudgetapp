cf7cee83f4b8372ed9767ffbb2d50223692e87ed
Vladislav Pauk <vladislavpauk@gmail.com>
2015-11-05 12:29:14 +0200

    Add edit and update actions to budget domain memberships controller

--- app/controllers/budget_domain_memberships_controller.rb
+++ app/controllers/budget_domain_memberships_controller.rb
@@ -8,6 +8,23 @@ class BudgetDomainMembershipsController < ApplicationController
     )
   end
 
+  def edit
+    @budget_domain = BudgetDomain.find(params[:budget_domain_id])
+    @membership = BudgetDomainMembership.find(params[:id])
+  end
+
+  def update
+    @budget_domain = BudgetDomain.find(params[:budget_domain_id])
+    @membership = BudgetDomainMembership.find(params[:id])
+    respond_to do |format|
+      if @membership.update(membership_params)
+        format.html { redirect_to budget_domain_memberships_path, notice: 'Budget domain was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   # GET /domains/memberships/join?token=XXXX.XXXX.XXXX
   def join
     JWTDecoder.call(token: params[:token]).resolve(
@@ -37,5 +54,11 @@ class BudgetDomainMembershipsController < ApplicationController
       format.html { redirect_to budget_domains_path, notice: 'Budget domain membership was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def membership_params
+    params.require(:budget_domain_membership).permit(:role)
   end
 end
