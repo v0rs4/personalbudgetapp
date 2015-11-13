5141fa15d28c38f355a7791eb6a15e409b466e2b
Vladislav Pauk <vladislavpauk@gmail.com>
2015-11-13 23:55:41 +0200

    Add budget categories population when create budget domain

--- app/models/budget_category.rb
+++ app/models/budget_category.rb
@@ -19,6 +19,54 @@ class BudgetCategory < ActiveRecord::Base
     :miscellaneous
   ]
 
+  HOME_EXPENSES = [
+    :mortage,
+    :rent,
+    :electricity,
+    :gas_oil,
+    :water_sewer_trash,
+    :other_utilities,
+    :mobile_telephone
+  ]
+
+  INCOME = [
+    :wages,
+    :interest_dividends,
+    :refunds_reimbursements,
+    :transfer_from_savings
+  ]
+
+  DAILY_LIVING = [
+    :groceries,
+    :personal_supplies,
+    :clothing,
+    :dining_out
+  ]
+
+  ENTERTAINMENT = [
+    :movies,
+    :videogames,
+    :books
+  ]
+
+  # MAP = Hash[
+  #   ENTERTAINMENT.product([:entertainment]) +
+  #   HOME_EXPENSES.product([:home_expenses]) +
+  #   INCOME.product([:income]) +
+  #   DAILY_LIVING.product([:daily_living])
+  # ]
+
+  SETS = lambda do
+    keys = [:name, :kind]
+    [
+      [ENTERTAINMENT, :entertainment],
+      [HOME_EXPENSES, :home_expenses],
+      [INCOME, :income],
+      [DAILY_LIVING, :daily_living]
+    ].flat_map { |a| a[0].product([a[1]]).map { |x| Hash[keys.zip(x)] } }
+  end.call
+
+
   enum kind: Hash[KINDS.map { |x| [x, x.to_s] }]
 
   validates :name, presence: true
--- app/services/budget_domain_creator.rb
+++ app/services/budget_domain_creator.rb
@@ -7,17 +7,38 @@ class BudgetDomainCreator
   attr_reader :budget_domain
   attr_reader :membership
 
+  # CATEGORIES_SETS = lambda do
+  #   keys = [:name,:kind]
+  #   [
+  #     [BudgetCategory::ENTERTAINMENT, :entertainment],
+  #     [BudgetCategory::HOME_EXPENSES, :home_expenses],
+  #     [BudgetCategory::INCOME, :income],
+  #     [BudgetCategory::DAILY_LIVING, :daily_livig]
+  #   ].flat_map { |a| a[0].product([a[1]]).map { |x| Hash[keys.zip(x)] } }
+  # end.call
+
   def call
     @budget_domain = BudgetDomain.new(params)
     BudgetDomain.transaction do
-      @budget_domain.memberships.build(
-        user_id: user_id,
-        role: :admin
-      )
+      build_membership
+      build_categories
       @budget_domain.save!
     end
     @status = :ok
   rescue StandardError
     @status = :failed
+  end
+
+  private
+
+  def build_membership
+    @budget_domain.memberships.build(
+        user_id: user_id,
+        role: :admin
+      )
+  end
+
+  def build_categories
+    @budget_domain.categories.build(BudgetCategory::SETS)
   end
 end
