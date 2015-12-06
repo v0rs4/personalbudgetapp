d4ad9ad7c67a263e643ea720d0b9fc8e741ab54d
Vladislav Pauk <vladislavpauk@gmail.com>
2015-12-06 16:29:24 +0200

    Alter api/v1/budget_transactions & corresponding serializer

--- app/controllers/api/v1/budget_transactions_controller.rb
+++ app/controllers/api/v1/budget_transactions_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::BudgetTransactionsController < Api::V1::BaseController
   authorize_resource :through => :budget_domain
 
   def index
-    @budget_transactions = BudgetTransaction.accessible_by(current_ability).includes(:budget_account)
+    sleep(3)
+    @budget_transactions = BudgetTransaction.accessible_by(current_ability).includes(:budget_account).includes(:budget_category).includes(:user)
     render json: @budget_transactions, each_serializer: Api::V1::BudgetTransactionSerializer
   end
 end
--- app/serializers/api/v1/budget_transaction_serializer.rb
+++ app/serializers/api/v1/budget_transaction_serializer.rb
@@ -1,5 +1,14 @@
 class Api::V1::BudgetTransactionSerializer < ActiveModel::Serializer
-  attributes :amount, :comment, :kind, :account_name
+  ATTRS = [
+    :amount,
+    :comment,
+    :kind,
+    :account_name,
+    :category_name,
+    :user_email
+  ]
+
+  attributes *ATTRS
 
   def kind
     object.source_table.sub('budget_'.freeze, ''.freeze).singularize
@@ -7,5 +16,13 @@ class Api::V1::BudgetTransactionSerializer < ActiveModel::Serializer
 
   def account_name
     object.budget_account.try(:name)
+  end
+
+  def category_name
+    object.budget_category.try(:name)
+  end
+
+  def user_email
+    object.user.try(:email)
   end
 end
